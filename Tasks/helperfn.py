fcef7ce7cdf2b8230c28f2d0b57c1919d6c7e6e7
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-10-10 13:44:45 +0100

    Updated docstrings and reapplied Kamils fix

--- Tasks/helperfn.py
+++ Tasks/helperfn.py
@@ -5,10 +5,10 @@ def _get_random_data(filepath):
     """Use get_random_data() instead. 
     Get Dataframe with randomized instance order, takes filepath as arg
 
-    :param filepath: [description]
-    :type filepath: [type]
-    :return: [description]
-    :rtype: [type]
+    :param filepath: The full absolute path to the file
+    :type filepath: string
+    :return: The entire data collection with randomized order of instances
+    :rtype: pandas datafram
     """
     df = pandas.read_csv(filepath, header='infer')
     return df.sample(frac=1)
@@ -17,21 +17,22 @@ def _get_random_data(filepath):
 def _get_file_path(filename):
     """Construct the filepath string, takes name of file as arg
 
-    :param filename: [description]
-    :type filename: [type]
-    :return: [description]
-    :rtype: [type]
+    :param filename: The name of the file to be imported
+    :type filename: string
+    :return: The absolute path to the file
+    :rtype: string
     """
     my_path = os.path.abspath(os.path.dirname("Data/"))
     return os.path.join(my_path, filename)
 
+# use this function to get the full csv
 def get_random_data(filename):
     """Get Dataframe with randomized instance order, takes filename as arg
 
-    :param filename: [description]
-    :type filename: [type]
-    :return: [description]
-    :rtype: [type]
+    :param filename: The name of the file to be imported
+    :type filename: string
+    :return: The entire data collection with randomized order of instances
+    :rtype: pandas dataframe
     """
-    return _get_random_data(_get_file_path('x_train_gr_smpl.csv'))
+    return _get_random_data(_get_file_path(filename))
 
