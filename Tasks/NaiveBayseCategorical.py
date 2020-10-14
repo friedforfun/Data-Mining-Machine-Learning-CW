dd87f3fee7091e53b209f7d2400217d1cd2f2773
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-10-14 15:14:47 +0100

    Imported NBC classifer to jupyternb

--- Tasks/NaiveBayseCategorical.py
+++ Tasks/NaiveBayseCategorical.py
@@ -4,26 +4,36 @@ from sklearn.model_selection import train_test_split
 import helperfn
 
 
-training_smpl = helperfn.get_data_noresults()
-raw_data_results = []
+def build_nbc_models(test_size=0.2, random_state=0):
+    """Build and score naive bayse categorical model
 
-train_test_results = []
-classifiers = []
-scores = []
-for i in range(-1, 10):
-    raw_data_sets = raw_data_sets + [helperfn.get_data(i)]
-    train_test_data = train_test_data + \
-        [train_test_split(raw_data_sets[i][0], raw_data_sets[i]
-                          [1], test_size=0.2, random_state=0)]
-    classifiers = classifiers + \
-        [CategoricalNB().fit(train_test_data[i][0], train_test_data[i][2])]
-    scores = scores + [(classifiers[i].score(train_test_data[i][0], train_test_data[i][2]),
-                        classifiers[i].score(train_test_data[i][1], train_test_data[i][3]))]
+    :param test_size: the percentage of the sample size to test with, defaults to 0.2
+    :type test_size: float, optional
+    :param random_state: the random seed, defaults to 0
+    :type random_state: int, optional
+    :return: Tuple of all scores and classifer
+    :rtype: Tuple
+    """
+    training_smpl = helperfn.get_data_noresults()
+    raw_data_results = []
 
-for i in range(len(scores)):
-    print("Scores for dataset: ", i-1)
-    print("Training data score: ", scores[i][0])
-    print("Testing data score: ", scores[i][1])
-    print("--------------------------------------")
+    train_test_data = []
+    classifiers = []
+    scores = []
+
+    for i in range(-1, 10):
+        raw_data_results = raw_data_results + [helperfn.get_results(i)]
+        train_test_data = train_test_data + [train_test_split(training_smpl, raw_data_results[i], test_size=test_size, random_state=random_state)]
+        classifiers = classifiers + [CategoricalNB().fit(train_test_data[i][0], train_test_data[i][2])]
+        scores = scores + [(classifiers[i].score(train_test_data[i][0], train_test_data[i][2]),
+                            classifiers[i].score(train_test_data[i][1], train_test_data[i][3]))]
+
+    for i in range(len(scores)):
+        print("Scores for dataset: ", i-1)
+        print("Training data score: ", scores[i][0])
+        print("Testing data score: ", scores[i][1])
+        print("--------------------------------------")
+
+    return classifiers, scores
 
 
