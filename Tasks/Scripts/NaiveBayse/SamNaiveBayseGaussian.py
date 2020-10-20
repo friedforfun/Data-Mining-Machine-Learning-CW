58f9194f64a7265b1a4d56c601169f1135a269c1
Sam Fay-Hunt <samfayhunt@gmail.com>
2020-10-20 22:12:22 +0100

    Nicely plot confusion matrices

--- Tasks/Scripts/NaiveBayse/SamNaiveBayseGaussian.py
+++ Tasks/Scripts/NaiveBayse/SamNaiveBayseGaussian.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import confusion_matrix, accuracy_score, ConfusionMatrixDisplay, cohen_kappa_score
+from sklearn.metrics import confusion_matrix, accuracy_score, ConfusionMatrixDisplay, cohen_kappa_score, plot_confusion_matrix
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import RFE
@@ -69,12 +69,15 @@ def build_confusion_matrix(classifiers, data):
     :param data: List of the Train and testing data
     :return: List: sklearn.metrics.confusion_matrix
     """
+    train_confusion = []
     confusion = []
     # build confusion matrices for all classifiers
     for i in range(len(classifiers)):
+        cmt = confusion_matrix(data[i][2], classifiers[i].predict(data[i][0]))
         cm = confusion_matrix(data[i][3], classifiers[i].predict(data[i][1]))
         confusion = confusion + [cm]
-    return confusion
+        train_confusion += [cmt]
+    return train_confusion, confusion
 
 def show_confusion_matrix(confusion, index_range=(0, 10), kappas=None):
     """Diplay all confusion matrix within range
@@ -84,23 +87,77 @@ def show_confusion_matrix(confusion, index_range=(0, 10), kappas=None):
     :param index_range: the range of indices within the confusion param to display, defaults to (0, 10)
     :type index_range: tuple(int), optional
     """
+    fig, axes = plt.subplots(nrows=len(confusion), figsize=(7, len(confusion)*10))
+    ax = axes.ravel()
     for i in range(index_range[0], index_range[1]):
         if kappas is not None:
-            print('Kappa: ', kappas[i])
-        fig, ax = plt.subplots()
-        cax = ax.matshow(confusion[i])
+            #print('Kappa: ', kappas[i])
+            kappa = round(kappas[i], 2)
         if i == 0:
             # special case labels
             cmd = ConfusionMatrixDisplay(confusion[i], display_labels=[i for i in range(0,10)])
-            plt.title("All classes")
+            cmd.plot(ax=ax[i])
+            if kappas is not None:
+                cmd.ax_.set_title(f'All classes\nKappa: {kappa}', fontsize=20)
+            else:
+                cmd.ax_.set_title('All classes', fontsize=20)
         else:
             cmd = ConfusionMatrixDisplay(
                 confusion[i], display_labels=['True', 'False'])
-            plt.title(label_def.get(i, i))
+            cmd.plot(ax=ax[i])
+            label = label_def.get(i, i)
+            if kappas is not None:
+                cmd.ax_.set_title(f'{label}\n Kappa: {kappa}', fontsize=20)
+            else:
+                cmd.ax_.set_title(f'{label}', fontsize=20)
+            
+    plt.subplots_adjust(wspace=0.40, hspace=0.1)
+    axes.flat[-1].set_visible(False)
+    plt.show()
 
-        cmd.plot(ax=ax)
-        plt.show()
-        
+def multi_show_confusion_matrix(conf_arr, index_range=(0,10), kappas=None, col_labels=None):
+    """Display all confusion matrices together
+
+    :param conf_arr: List containing arrays of confusion matrices
+    :type conf_arr: List[List[numpy.array]]
+    :param index_range: the range of indices within the confusion param to display, defaults to (0, 10), defaults to (0,10)
+    :type index_range: tuple(int), optional
+    :param kappas: List of Kappa values matching the shape of conf_arr, defaults to None
+    :type kappas: List[List[numpy.array]], optional
+    :param col_labels: Description of each dataset
+    :type col_labels: List[string]
+    """
+    if kappas is not None:
+        if len(conf_arr) != len(kappas):
+            raise ValueError('mismatched conf_arr and kappas params')
+
+    # all i in conf_arr[i] must have equal length
+    fig, axes = plt.subplots(nrows=len(conf_arr[0]), ncols=len(conf_arr), figsize=(len(conf_arr)*10, len(conf_arr[0])*10))
+    ax_arr = axes.ravel()
+
+    for i in range(len(conf_arr)):
+        for j in range(len(conf_arr[i])):
+            if j == 0:
+                disp_labels = [i for i in range(0, 10)]
+                title = f'{col_labels[i]}\nAll classes'
+                if kappas is not None:
+                    kappa = round(kappas[i][j], 2)
+                    title = f'{col_labels[i]}\nAll classes\nKappa: {kappa}'
+            else:
+                disp_labels = ['True', 'False']
+                label = label_def.get(j-1, j-1)
+                title = label
+                if kappas is not None:
+                    kappa = round(kappas[i][j], 2)
+                    title = f'{label}\nKappa: {kappa}'
+            cmd = ConfusionMatrixDisplay(conf_arr[i][j], display_labels=disp_labels)
+            cmd.plot(ax=ax_arr[j*len(conf_arr)+i])
+            cmd.ax_.set_title(title, fontsize=20)
+
+
+    plt.tight_layout(h_pad=5, w_pad=2)
+    
+    plt.show()
 
 def kappa(confusion):
     """Generate kappa values for the confusion matrix against randomised ndarrays
