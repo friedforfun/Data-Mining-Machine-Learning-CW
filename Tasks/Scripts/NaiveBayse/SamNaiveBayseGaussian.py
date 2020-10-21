4bc48adac57bba0d939e330741bbbd79ef65ce62
Lewis <Lewiswilson1497@gmail.com>
2020-10-21 15:30:15 +0100

    Updated printing

--- Tasks/Scripts/NaiveBayse/SamNaiveBayseGaussian.py
+++ Tasks/Scripts/NaiveBayse/SamNaiveBayseGaussian.py
@@ -73,36 +73,35 @@ def build_nbg_models(test_size=0.2, random_state=0, balance_classes=False):
 
 def rskSplit():
     
-    from sklearn.model_selection import RepeatedStratifiedKFold
+    #from sklearn.model_selection import RepeatedStratifiedKFold
     
     classifiers = []
     scores = []
-    X , y = helperfn.get_data(2)
-
     
-    rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=2,random_state=36851234)
-        
-    for train_index, test_index in rskf.split(X, y):
-        print("TRAIN:", train_index, "TEST:", test_index)
-        print(train_index.shape , test_index.shape)
-        X_train, X_test = np.take(X,train_index, axis=0), np.take(X,test_index, axis=0)
-        y_train, y_test = np.take(y,train_index, axis=0), np.take(y,test_index, axis=0)
-        
-        
-    for i in range(0, 11):
+    for i in range(0,11):
     
-        classifiers = classifiers + \
-                    [GaussianNB().fit(X_train.values,
-                                    y_train.values)]
-        scores = scores + [(classifiers[i].score( X_train, y_train),
-                                    classifiers[i].score(X_test, y_test))]
+        X , y = helperfn.get_data(i)
 
-        for i in range(len(scores)):
+        
+        rskf = RepeatedStratifiedKFold(n_splits=2, n_repeats=2,random_state=36851234)
+            
+        for train_index, test_index in rskf.split(X, y):
+            X_train, X_test = np.take(X,train_index, axis=0), np.take(X,test_index, axis=0)
+            y_train, y_test = np.take(y,train_index, axis=0), np.take(y,test_index, axis=0)
+            
+            
+            classifiers = classifiers + \
+                        [GaussianNB().fit(X_train.values,
+                                        y_train.values)]
+            scores = scores + [(classifiers[i].score( X_train, y_train),
+                                        classifiers[i].score(X_test, y_test))]
+
+       
             print("Scores for dataset: ", label_def.get(i-1, i-1))
             print("Training data score: ", scores[i][0])
             print("Testing data score: ", scores[i][1])
             print("--------------------------------------")
-        
+            
 
 
 def build_confusion_matrix(classifiers, data):
