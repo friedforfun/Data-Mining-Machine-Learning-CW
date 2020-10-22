bd4d9e932efa9688afde639707c5fd0ecfe0c56a
jimmym620 <jimmym620@gmail.com>
2020-10-22 13:33:09 +0100

    Try fix git

---
+++ Tasks/Scripts/pixelFinder.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def bestPixels(X, y, n):
+    """Get n number of best pixels indexes
+    :param X: Dataframe of features
+    :type X: dataframe
+    :param y: Dataframe of outcome
+    :type y: dataframe
+    :return: Array of size n with idexes of n best pixels
+    :rtype: array
+    """
+    result = []
+
+    for i in range(X.shape[1]):
+        result.append(X[str(i)].corr(y['y']))
+
+    result = np.array(result)
+
+
+    res_arr = []
+
+    for i in range(n):
+
+        index = np.argmax(result)
+        res_arr.append(index)
+
+        result = np.delete(result, index)
+
+
+    return res_arr
